cc383243ad2c37cbdb34994c5d399f622cdad6ae
zoka <zokaxxdmenendes@gmail.com>
2026-05-28 11:42:09 -0300

    Post user

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
-from fastapi import FastAPI, HTTPException, depends
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, EmailStr
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(
@@ -46,4 +46,22 @@ def get_db():
 @app.get("/")
 def home():
     return {"message": "Welcome to the To Do List API!"}
+
+@app.post("/users")
+def create_user(User: User):
+    conn, cursor = get_db()
+
+    cursor.execute("SELECT * FROM users WHERE email = ?", (User.email,))
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        conn.close()
+        raise HTTPException(status_code=400, detail="Email already exists")
     
+    cursor.execute("INSERT INTO users (name, age, email, password) VALUES (?, ?, ?, ?)",
+                (User.name, User.age, User.email, User.password)
+            )
+    conn.commit()
+    conn.close()
+
+    return {"message": "User created successfully!"}
